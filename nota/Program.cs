0110b761e4d585a032af2016a180f4156de59ed6
brand <brand@CRISTOPHE_M_L>
2024-06-06 16:37:33 -0400

    Agregar archivos de proyecto.

---
+++ nota/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace nota
+{
+    class progra
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(" \n Este es un programa de notas\n ");
+
+            int nota;
+
+            Console.WriteLine("\n Ingrese la nota\n ");
+            nota = int.Parse(Console.ReadLine());
+
+            
+            if (nota > 100)
+            {
+                Console.WriteLine("La nota ingresada No es valida");
+            }
+            else
+            {
+                if (nota >= 90)
+                {
+                    Console.WriteLine("Exelente");
+                }
+                else
+                {
+                    if (nota >= 80)
+                    {
+                        Console.WriteLine("Muy bien");
+                    }
+                    else
+                    {
+                        if (nota >= 70)
+                        {
+                            Console.WriteLine("Bien");
+                        }
+                        else
+                        {
+                           if(nota < 70)
+                            {
+                                Console.WriteLine("Reprobado");
+                            }
+                        }
+                       
+                    }
+                }
+              
+            }
+            
+        }
+       
+    }
+}
+
